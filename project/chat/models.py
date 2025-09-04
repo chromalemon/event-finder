8d12217f7ebd06767a016b0e0dcf60b18c1301a8
chromalemon <19nmelnichenko@chelsea-academy.org>
2025-09-04 19:48:30 +0100

    Wrote Pseudocode

--- project/chat/models.py
+++ project/chat/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from events.models import Event
 
 # Create your models here.
+
+class GroupChat(models.Model):
+    event = models.ForeignKey("events.Event", on_delete=models.CASCADE, related_name='group_chats')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class GroupChatMessage(models.Model):
+    chat = models.ForeignKey("GroupChat", on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE, related_name='group_chat_messages')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Message from {self.user.username} in chat {self.chat.id}"
--- project/events/models.py
+++ project/events/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Event(models.Model):
-    event_id = models.AutoField(primary_key=True)
     host = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE, related_name='events')
     title = models.CharField(max_length=200)
     description = models.TextField()
