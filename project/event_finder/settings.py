da00a176b08959160132619d76fb33a6766e3958
Nathaniel Melnichenko <19nmelnichenko@chelsea-academy.org>
2025-11-17 12:20:55 +0000

    Fixed static fetch

--- project/event_finder/settings.py
+++ project/event_finder/settings.py
@@ -141,8 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [    
-    BASE_DIR / 'static',
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
 ]
 
 # Default primary key field type
